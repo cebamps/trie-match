d62ea998a226adf3c9a5bc6573b3047d6add30f5
Cédric Bamps <12797742+cebamps@users.noreply.github.com>
2025-01-19 22:01:44 +0100

    add some rudimentary cli stuff to main

--- app/CLI.hs
+++ app/CLI.hs
@@ -1,16 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 module CLI where
 
 import qualified Data.Text as T (lines, splitOn)
 import qualified Data.Text.IO as T
-import Trie (buildLiteralTrie, dump)
+import Trie (buildLiteralTrie, dump, buildPatternTrie)
+import Data.Text (Text)
+import System.Environment (getArgs)
+import Parse (parsePattern)
 
 run :: IO ()
 run = do
-  let getPatterns = fmap (T.splitOn ".") . T.lines <$> T.getContents
-  queryTrie <- buildLiteralTrie <$> getPatterns
+  (arg1, arg2) <- getTwoArgs
+  
+  queryTrie <- buildLiteralTrie . fmap (T.splitOn ".") . T.lines <$> readFrom arg1
+  patternTrie <- do
+    raw <- T.lines <$> readFrom arg2
+    parsed <- liftEither $ traverse parsePattern raw
+    return $ buildPatternTrie parsed
+
   putStrLn $ dump queryTrie
+  putStrLn $ dump patternTrie
+
   where
     liftEither :: (Show e) => Either e a -> IO a
     liftEither = either (fail . show) return
+    getTwoArgs :: IO (String, String)
+    getTwoArgs = getArgs >>= \case
+      [x,y] -> return (x,y)
+      _ -> fail "Expected two arguments"
+
+readFrom :: FilePath -> IO Text
+readFrom "-" = T.getContents
+readFrom p = T.readFile p
