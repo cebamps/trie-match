758e15039c7882fc67b1863c39f5d9e89ba038ca
Cédric Bamps <12797742+cebamps@users.noreply.github.com>
2025-01-28 18:52:29 +0100

    refactor search to make state machine more visible

--- src/Search.hs
+++ src/Search.hs
@@ -14,28 +14,28 @@ searchLit = go ([], [])
 
 go :: ([PatternSegment], [Text]) -> Trie PatternSegment a -> Trie Text b -> [(Pattern, [Text])]
 go (ppath, npath) patterns needles = do
+  -- pick all branch pairs
   (nk, needles') <- children needles
   (pk, patterns') <- children patterns
   let ppath' = pk : ppath
       npath' = nk : npath
-  guard $ patMatch pk nk
 
-  let matched = do
+  let matched = maybeToList $ do
         -- uses MonadFail instance; TODO return value too
         nMatch <- tValue needles'
         pMatch <- tValue patterns'
         pure (reverse ppath', reverse npath')
 
-  -- A match against PPlus is allowed to not consume the pattern segment, so that it spans one or more needle segments. This is implemented by sythesizing an additional pattern trie with just the PPlus branch.
-  let notMoving = do
-        guard $ pk == PPlus
-        starTrie <- maybeToList $ justChild PPlus patterns
-        go (tail ppath', npath') starTrie needles'
+  -- State transitions walking dawn the needle trie or both. When we stay
+  -- put, we make sure to prune the trie to just the branch we've picked.
+  let popBoth = matched <|> go (ppath', npath') patterns' needles'
+      popNeedle = do
+        patterns'' <- maybeToList $ justChild pk patterns
+        go (ppath, npath') patterns'' needles'
 
-  let moving =
-        go (ppath', npath') patterns' needles'
-
-  maybeToList matched <|> moving <|> notMoving
-  where
-    patMatch PPlus _ = True
-    patMatch (PGlob g) t = globMatch g t
+  case pk of
+    -- A match against PPlus is allowed to not consume the pattern segment,
+    -- so that it spans one or more needle segments. This is implemented by
+    -- sythesizing an additional pattern trie with just the PPlus branch.
+    PPlus -> popBoth <|> popNeedle
+    PGlob g -> guard (globMatch g nk) >> popBoth
